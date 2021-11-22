e5daa236086dbe4c52fca0a0c6c069ed2936ac46
yeyee2901 <gabriel777sh@gmail.com>
2021-11-22 10:00:58 +0700

    ADD: workaround to avoid page transition glitch

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,19 @@ import About from './pages/about'
 import Works from './pages/works'
 import { Routes, Route, useLocation } from 'react-router-dom'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import { Box } from '@chakra-ui/react'
+import { useEffect } from 'react'
 
 const Main = () => {
-  const current_location = useLocation()
+  const location = useLocation()
+
+  // to check if route is changed
+  // this will avoid visual bug that I encounter
+  // during page transition
+  const on_page_change = () => {
+    scroll(0, 0)
+  }
+  useEffect(on_page_change, [location.pathname])
 
   return (
     <>
@@ -16,7 +26,7 @@ const Main = () => {
         <CSSTransition
           timeout={250}
           classNames="fade"
-          key={current_location.key}
+          key={location.key}
         >
           <Routes>
             <Route path="/" element={<Layout />} >
