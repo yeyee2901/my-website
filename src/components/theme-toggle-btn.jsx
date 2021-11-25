d854a56742f789d80b6f3fdd1dcc5f796e2ccebc
yeyee2901 <gabriel777sh@gmail.com>
2021-11-25 20:58:44 +0700

    ADD: animation on color change

--- src/components/theme-toggle-btn.jsx
+++ src/components/theme-toggle-btn.jsx
@@ -1,6 +1,7 @@
 import { Button, useColorModeValue, useColorMode } from '@chakra-ui/react'
 import { SunIcon, MoonIcon } from '@chakra-ui/icons'
 import MyColors from '../colors'
+import { motion, AnimatePresence } from 'framer-motion'
 
 const ThemeToggleButton = ({ children, ...props }) => {
   const { toggleColorMode } = useColorMode()
@@ -10,21 +11,31 @@ const ThemeToggleButton = ({ children, ...props }) => {
   const fg = useColorModeValue(MyColors.dark.fg, MyColors.light.fg)
 
   return (
-    <Button
-      p={2}
-      maxH="40px"
-      maxW="40px"
-      bg={bg}
-      color={fg}
-      onClick={toggleColorMode}
-      {...props}
-      _hover={{
-        bg: MyColors.dark.bg2,
-        color: MyColors.yellow
-      }}
-    >
-      {icon}
-    </Button>
+    <AnimatePresence exitBeforeEnter initial={false}>
+      <motion.div
+        key={useColorModeValue('light', 'dark')}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ scale: 300, opacity: 0 }}
+        transition={{ delay: 0, duration: 1 }}
+      >
+        <Button
+          p={2}
+          maxH="40px"
+          maxW="40px"
+          bg={bg}
+          color={fg}
+          onClick={toggleColorMode}
+          {...props}
+          _hover={{
+            bg: MyColors.dark.bg2,
+            color: MyColors.yellow
+          }}
+        >
+          {icon}
+        </Button>
+      </motion.div>
+    </AnimatePresence>
   )
 }
 
